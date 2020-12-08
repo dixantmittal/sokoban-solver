d1b0177f8ca636a027c15abffad7b4cf55082f39
Dixant Mittal <dixant.mittal@gmail.com>
2020-12-08 15:34:50 +0800

    adds java compiling instructions in parallel.py

--- parallel.py
+++ parallel.py
@@ -12,9 +12,11 @@ print("Starting index: ", int(sys.argv[1]))
 print("Ending index: ", int(sys.argv[2]))
 print("Number of workers: ", int(sys.argv[3]))
 
+print("Compiling classes")
+os.system("javac -d bin src/*.java")
+
 commands = [
-    "java -jar bin/SokobanSolver.jar levels/{:03d}.txt experts/{:03d}.txt".
+    "java -classpath bin DataGenerator levels/{:03d}.txt experts/{:03d}.txt".
     format(i, i) for i in range(int(sys.argv[1]), int(sys.argv[2]))
 ]
-print(commands)
 pool = Pool(processes=int(sys.argv[3])).map(func=execute, iterable=commands)
