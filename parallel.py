a62ed7f67b7496b298f14e3796a04f5b4f77f73b
Dixant Mittal <dixant.mittal@gmail.com>
2021-01-02 14:10:15 +0800

    adds hard levels

--- parallel.py
+++ parallel.py
@@ -12,11 +12,10 @@ print("Starting index: ", int(sys.argv[1]))
 print("Ending index: ", int(sys.argv[2]))
 print("Number of workers: ", int(sys.argv[3]))
 
-#print("Compiling classes")
-#os.system("javac -d bin src/*.java")
+print("Compiling classes")
+os.system("javac -d bin src/*.java")
 
 commands = [
-    "java -classpath bin DataGenerator levels/{:03d}.txt experts/{:03d}.txt".
-    format(i, i) for i in range(int(sys.argv[1]), int(sys.argv[2]))
+    "java -classpath bin DataGenerator levels_hard/{:03d}.txt experts_hard/{:03d}.txt".format(i, i) for i in range(int(sys.argv[1]), int(sys.argv[2]))
 ]
 pool = Pool(processes=int(sys.argv[3])).map(func=execute, iterable=commands)
